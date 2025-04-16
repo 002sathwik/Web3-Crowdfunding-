fab46727b651abd2fea26c02c656f74271d88428
Sathwik <hsathwik002@gmail.com>
2025-04-16 19:04:25 +0530

    [feath]:Final

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -1,23 +1,4 @@
-// const hre = require("hardhat");
 
-// async function main() {
-//   const [deployer] = await hre.ethers.getSigners();
-
-//   console.log("Deploying contracts with account:", deployer.address);
-
-//   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-//   const crowdFunding = await CrowdFunding.deploy();
-
-//   // Wait for the transaction to be mined
-//   const tx = await crowdFunding.deployTransaction.wait();
-
-//   console.log("CrowdFunding deployed to:", crowdFunding.address);
-// }
-
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
 const hre = require("hardhat");
 
 async function main() {
@@ -26,9 +7,9 @@ async function main() {
   console.log("Deploying contracts with account:", deployer.address);
 
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-  const crowdFunding = await CrowdFunding.deploy(); // Automatically waits for deployment in v6
+  const crowdFunding = await CrowdFunding.deploy();
 
-  console.log("CrowdFunding deployed to:", crowdFunding.target); // use `.target` instead of `.address` in ethers v6
+  console.log("CrowdFunding deployed to:", crowdFunding.target);
 }
 
 main().catch((error) => {
